1d2cf48ddb3cf14494ece23eaa89c5f2a8d9473b
robert <robertmutua196@gmail.com>
2024-09-22 20:30:19 +0300

    Proj 2

---
+++ SQL_Proj_2/Extract_Year.sql
@@ -0,0 +1,9 @@
+SELECT
+    job_title_short AS 'Job Title',
+    job_location AS Location,
+    Job_posted_date AS Date,
+    EXTRACT(YEAR FROM Job_posted_date) AS YEAR
+FROM
+    job_postings_fact
+LIMIT 100
+
