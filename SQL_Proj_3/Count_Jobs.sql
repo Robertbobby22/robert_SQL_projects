ae8bad7ed8dd477e26e7b55113e8dd5e911e731f
robert <robertmutua196@gmail.com>
2024-09-22 21:35:52 +0300

    Count Number of Jobs Every Month

--- SQL_Proj_3/Count_Jobs.sql
+++ SQL_Proj_3/Count_Jobs.sql
@@ -1,10 +1,11 @@
 SELECT
-   COUNT(job_id) AS 'Number of Jobs',
+   COUNT(job_id) AS Number_of_Jobs,
    EXTRACT(MONTH FROM Job_posted_date) AS Month
 FROM
    job_postings_fact
 GROUP BY
    Month
-
+ORDER BY
+   Number_of_Jobs DESC;
 
 
