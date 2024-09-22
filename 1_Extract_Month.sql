76a4a7abb9ca341f7e6b90f3121ae289389c673e
Breezy <125439943+Robertbobby22@users.noreply.github.com>
2024-09-22 18:12:13 +0300

    Delete 1_Extract_Month.sql

--- 1_Extract_Month.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-   job_title_short AS 'Job Title',
-   job_location AS 'Job Location',
-   Job_posted_date AS Date,
-   EXTRACT(MONTH FROM Job_posted_date) AS MONTH
-FROM job_postings_fact
-LIMIT 100
-
