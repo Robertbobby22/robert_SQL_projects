bca9bb2e0b2dcd0e8bae5d1dff841be724e1c7c4
robert <robertmutua196@gmail.com>
2024-09-22 21:25:13 +0300

    Proj 3

--- SQL_Proj_3/Count_Jobs.sql
+++ SQL_Proj_3/Count_Jobs.sql
@@ -1,8 +1,10 @@
 SELECT
-   COUNT(job_id) AS 'Number of Jobs'
+   COUNT(job_id) AS 'Number of Jobs',
+   EXTRACT(MONTH FROM Job_posted_date) AS Month
 FROM
    job_postings_fact
-LIMIT 100
+GROUP BY
+   Month
 
 
 
